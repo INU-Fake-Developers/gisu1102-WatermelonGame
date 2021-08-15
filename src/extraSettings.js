8a32a0b48132ba0bbce778c761b0244ef6ba947c
gisu1102 <76506573+gisu1102@users.noreply.github.com>
2021-08-15 18:26:18 +0900

    Fix::iss06
    .

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -1,5 +1,5 @@
 /**
- * 声明，本项目仅帮助大家学习技术及娱乐，切勿将修改后的网站大规模传播及商用，以避免侵权！
+ * 성명서, 이 프로젝트는 여러분이 기술 및 엔터테인먼트에 대해서만 학습할 수 있도록 도와줄 것이며, 절대 수정된 사이트의 대규모 전파 및 상용화를 방지하여, 특허 침해를 방지할 것입니다!
  */
 
 // 추가 점수: 숫자 수정
@@ -16,7 +16,7 @@ let reverseLevelUp = false;
 
 // 생성된 과일 지정: 기본값: 반전 0-5 켜지 않고 반전 6-11 켜고, 숫자 수정으로 무작위로 생성된 과일 범위 제어
 const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 무작위 과일 생성 최소치(0-10) 0은 포도, 9는 수박 반쪽
-const maxRandomFruitNum = reverseLevelUp ? 11 : 2; // 생성된 임의과일 최대치(1-11) 0은 포도, 9는 수박 반개
+const maxRandomFruitNum = reverseLevelUp ? 11 : 5; // 생성된 임의과일 최대치(1-11) 0은 포도, 9는 수박 반개
 let setFruits = {
   //지정 전에 몇 번 생성된 과일은 임의의 수량을 채울 수 있습니다. 0은 포도, 9는 수박 반 개입니数（推荐 0.9）다.
   startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [0, 0, 1, 2, 2, 3],
@@ -26,10 +26,10 @@ let setFruits = {
 }
 
 // 과일 탄성 더하기: false를 1보다 작은 임의의 소수로 바꿉니다 (0.9 추천)
-let fruitQTan = 0.9;
+let fruitQTan = 0.3;
 
 // 과일 낙하를 더디게 한다: false를 0보다 많은 임의의 수로 변경한다. 값이 클수록 저항이 더디게 내린다(5 추천)
-let fruitSlowDown = 5;
+let fruitSlowDown = 0.5;
 
 // 오른쪽 위 아이콘을 클릭하여 과일 바꾸기: false를 true로 변경하면 됩니다
 let clickChangeFruit = true;
